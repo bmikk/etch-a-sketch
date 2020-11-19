55d40a8aa9202e67cea0c3ea8ad29197087d5b6c
bmikk <54866601+bmikk@users.noreply.github.com>
2020-11-19 09:47:45 -0700

    started css

--- scripts.js
+++ scripts.js
@@ -13,29 +13,42 @@ const button2 = document.querySelector('#btn2');
 button2.addEventListener('click', deleteGrid());
 
 
-//what's the logic for function needed to create the grid?
-    //well, it would need to take X, and create a line of X divs, X times.
+
 
 let deleteGrid = () => {
         while (gridContainer.firstChild) {
             gridContainer.removeChild(gridContainer.firstChild);
         }
     }
+
     
-//So we create the function
-function createGrid (x) {
-    //now, start a loop that will append child elements to this first div
-    for (let i = 0; i < x; i++) { //this loop creates a number of divs, with each div serving as the first in the line
-        //create a div, append it to our root div
-        let vertDiv = document.createElement('div');
-        gridContainer.appendChild(vertDiv);
-        for (let y = 0; y < x; y++) { //create (x - 1) divs, each nested into the previous parent div and arranged in a line
-            //create a div, append it to "vertDiv"
-            let horDiv = document.createElement('div');
-            vertDiv.appendChild(horDiv);
-        }         
-    }
-    //and that's it? We have all our divs? YEP!
+function createGrid(input) {
+    for (let y = 0; y < input; y++) { //create (y) divs, each nested into the grid container
+        //create a div, append it to grid container
+        let gridItem = document.createElement('div');
+        gridContainer.appendChild(gridItem);
+        } 
 }
+
+
+
+
+
+
+////So we create the function
+//function createGrid (x) {
+//    //now, start a loop that will append child elements to this first div
+//    for (let i = 0; i < x; i++) { //this loop creates a number of divs, with each div serving as the first in the line
+//        //create a div, append it to our root div
+//        let vertDiv = document.createElement('div');
+//        gridContainer.appendChild(vertDiv);
+//        for (let y = 0; y < x; y++) { //create (x - 1) divs, each nested into the previous parent div and arranged in a line
+//           //create a div, append it to "vertDiv"
+//            let horDiv = document.createElement('div');
+//            vertDiv.appendChild(horDiv);
+//        }         
+//    }
+//    //and that's it? We have all our divs? YEP!
+//}
 
 //need a function that will delete whatever grid that's currently on the page
