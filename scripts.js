12ad0586f16832263573fcecbff7a913e027a8e0
Byohazyrd <benjaminmikkelson@gmail.com>
2020-11-21 13:15:49 -0700

    learning grid manipulation

--- scripts.js
+++ scripts.js
@@ -1,54 +1,43 @@
-//ok, here we go! what are our guidelines here?
-//Grid needs to be flexible, so that number of squares created will be variable depending on user input, but starting with 16x16
-//This means that we need a function that can be called to make the divs
-//need a button that will clear the CSS changes caused by the mouse hovering, creating a blank canvas of the same squares again
-//also need a button that will clear the grid, and then call for a new grid using the user's input to get the number of squares
-//crucially here, need to decide on the exact specifications of what a square will be
+
 const gridContainer = document.querySelector('#gridContainer');
 
 const button = document.querySelector('#btn');
-button.addEventListener('click', createGrid(prompt("How big should the grid be? Enter a number between 0-100.")));
+button.addEventListener('click', createNewGrid);
 
 const button2 = document.querySelector('#btn2');
-button2.addEventListener('click', deleteGrid());
+button2.addEventListener('click', deleteGrid);
 
-
-
-
-let deleteGrid = () => {
-        while (gridContainer.firstChild) {
-            gridContainer.removeChild(gridContainer.firstChild);
-        }
-    }
 
     
-function createGrid(input) {
-    for (let y = 0; y < input; y++) { //create (y) divs, each nested into the grid container
+function createFirstGrid() {
+    for (let i = 0; i < 16*16; i++) {
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        gridContainer.appendChild(gridItem);
+    }
+}
+
+
+function createNewGrid() {
+    let value = prompt("How many squares should one side of the new grid be?");
+    //now we need to re-size the grid based on the input
+    gridContainer.setAttribute('style', 'grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);');
+    for (let y = 0; y < value*value; y++) { //create (y) divs, each nested into the grid container
         //create a div, append it to grid container
         let gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
         gridContainer.appendChild(gridItem);
         } 
 }
 
 
+function deleteGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+createFirstGrid();
 
 
 
-
-////So we create the function
-//function createGrid (x) {
-//    //now, start a loop that will append child elements to this first div
-//    for (let i = 0; i < x; i++) { //this loop creates a number of divs, with each div serving as the first in the line
-//        //create a div, append it to our root div
-//        let vertDiv = document.createElement('div');
-//        gridContainer.appendChild(vertDiv);
-//        for (let y = 0; y < x; y++) { //create (x - 1) divs, each nested into the previous parent div and arranged in a line
-//           //create a div, append it to "vertDiv"
-//            let horDiv = document.createElement('div');
-//            vertDiv.appendChild(horDiv);
-//        }         
-//    }
-//    //and that's it? We have all our divs? YEP!
-//}
-
-//need a function that will delete whatever grid that's currently on the page
