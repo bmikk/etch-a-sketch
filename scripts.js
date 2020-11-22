d86b432c004b7196f1eafce2e533bbb6646ec153
Byohazyrd <benjaminmikkelson@gmail.com>
2020-11-21 22:36:55 -0700

    fixed functions, figured out css manipulation

--- scripts.js
+++ scripts.js
@@ -2,40 +2,52 @@
 const gridContainer = document.querySelector('#gridContainer');
 
 const button = document.querySelector('#btn');
-button.addEventListener('click', createNewGrid);
+button.addEventListener('click', clearAndCreate);
+
 
 const button2 = document.querySelector('#btn2');
 button2.addEventListener('click', deleteGrid);
 
+const button3 = document.querySelector('#btn3');
+
 
     
-function createFirstGrid() {
+function createFirstGrid() { //Creates initial grid of 16x16 squares.
     for (let i = 0; i < 16*16; i++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('gridItem');
+        gridItem.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = 'black'));
         gridContainer.appendChild(gridItem);
     }
 }
 
 
-function createNewGrid() {
+function createNewGrid() { //Takes user prompt, resizes the grid, and then fills it with divs
     let value = prompt("How many squares should one side of the new grid be?");
-    //now we need to re-size the grid based on the input
-    gridContainer.setAttribute('style', 'grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);');
-    for (let y = 0; y < value*value; y++) { //create (y) divs, each nested into the grid container
-        //create a div, append it to grid container
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);`);
+    for (let y = 0; y < value*value; y++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('gridItem');
+        gridItem.addEventListener('mouseover', (e) => (e.target.style.backgroundColor = 'black'));
         gridContainer.appendChild(gridItem);
         } 
 }
 
 
-function deleteGrid() {
+function deleteGrid() { //clears the grid
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
 }
+
+
+
+
+function clearAndCreate () { //clunky way to combine the two functions for a single event listener
+    deleteGrid();
+    createNewGrid();
+}
+
 
 createFirstGrid();
 
